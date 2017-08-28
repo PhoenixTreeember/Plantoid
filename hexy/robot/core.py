9ec84b77ee70307fc1bcdfbfdb759cc9eaad3082
Daniel Slåttnes <daniel.slaattnes@gmail.com>
2017-08-28 03:42:22 +0200

    Update core.py

--- hexy/robot/core.py
+++ hexy/robot/core.py
@@ -88,8 +88,8 @@ class Leg:
 
     def __init__(self, name, hip_key, knee_key, ankle_key):
 
-        max_hip, max_knee, max_ankle = 35, 150, 90
-        min_hip, min_knee, min_ankle = -35, -90, -90
+        max_hip, max_knee, max_ankle = 30, 70, 90
+        min_hip, min_knee, min_ankle = -30, -110, -90
         
         self.hip = Joint("hip", hip_key, max_hip, min_hip)
         self.knee = Joint("knee", knee_key, max_knee, min_knee)
